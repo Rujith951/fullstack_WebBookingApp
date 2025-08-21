1dcb9c0ce566dbf34efd01cd45b4f786eb822652
Rujith951 <rujithramu3951@gmail.com>
2025-08-21 16:34:21 +0530

    hotels api  functionalities are completed

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -9,3 +9,45 @@ export const addHotel = async (req, res, next) => {
 		next(err);
 	}
 };
+
+export const updateHotel = async (req, res, next) => {
+	try {
+		const updatedHotel = await Hotel.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.send(updatedHotel);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const deleteHotel = async (req, res, next) => {
+	try {
+		await Hotel.findByIdAndDelete(req.params.id);
+		res.send("Hotel deleted successfully");
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getHotel = async (req, res, next) => {
+	try {
+		const hotel = await Hotel.findById(req.params.id);
+		res.send(hotel);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const getHotels = async (req, res, next) => {
+	try {
+		const hotels = await Hotel.find();
+		res.send(hotels);
+	} catch (err) {
+		next(err);
+	}
+};
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,19 +1,21 @@
 import express from "express";
-import { addHotel } from "../controllers/hotel.js";
+import {
+	addHotel,
+	deleteHotel,
+	getHotel,
+	getHotels,
+	updateHotel,
+} from "../controllers/hotel.js";
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-	res.send("Hi i am get all hotels route");
-});
+router.get("/", getHotels);
 
-router.get("/:id", (req, res, next) => {
-	res.send("Hi i am get hotels by id route");
-});
+router.get("/:id", getHotel);
 
 router.post("/", addHotel);
 
-router.delete("/:id", (req, res, next) => {
-	res.send("Hi i am delete post hotels by id route");
-});
+router.put("/:id", updateHotel);
+
+router.delete("/:id", deleteHotel);
 
 export default router;
